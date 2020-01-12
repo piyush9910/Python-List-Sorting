4b5957d21ba031816d9207f46526a39ba002f19b
piyush9910 <54396900+piyush9910@users.noreply.github.com>
2020-01-12 13:46:48 +0530

    Add files via upload

---
+++ Insertion Sort.py
@@ -0,0 +1,10 @@
+a = [16, 19, 11, 15, 10, 12, 14]
+for i in a:
+    j = a.index(i)
+    while j>0:
+        if a[j-1] > a[j]:
+            a[j-1],a[j] = a[j],a[j-1]
+        else:
+            break
+        j = j-1
+print (a)
